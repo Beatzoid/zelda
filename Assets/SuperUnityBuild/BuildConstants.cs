a3d48c55023cde8eabcb30949939fac017844407
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-05-22 14:45:44 -0700

    ✨ Add SuperUnityBuild

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,39 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Test_Build,
+    }
+
+    public enum Platform
+    {
+        None,
+        macOS,
+        Linux,
+        PC,
+    }
+
+    public enum Architecture
+    {
+        None,
+        macOS,
+        Linux_x64,
+        Windows_x86,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637572902348944760);
+    public const string version = "1.0.0.1";
+    public const ReleaseType releaseType = ReleaseType.Test_Build;
+    public const Platform platform = Platform.PC;
+    public const Architecture architecture = Architecture.Windows_x86;
+    public const Distribution distribution = Distribution.None;
+}
+
